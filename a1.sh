d9e55d7034425e1fb8c9c59585ba49f21198763a
Ubuntu <ubuntu@ip-172-31-27-90.ap-south-1.compute.internal>
2020-11-17 15:13:29 +0000

    This should error out

--- a1.sh
+++ a1.sh
@@ -6,3 +6,4 @@ git branch -M main
 git remote add origin https://github.com/andalike/githubcollab.git
 git push -u origin main
 echo 'change by soni'                
+echo 'This line was changed by soni also'
